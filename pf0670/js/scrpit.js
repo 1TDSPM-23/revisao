84c3027006668f8554a6a13a75b6b62ae4ed9b6e
ousadoo <pedro.gomesf2004@gmail.com>
2023-08-04 09:32:59 -0300

    declarando arrays e atualizando

---
+++ pf0670/js/scrpit.js
@@ -0,0 +1,34 @@
+// // console.log("hello world!")
+// let alunos = [
+//     "João", "Jorge", "Pedro", "Robson", "Mariana", "Enzo", "roberval"
+// ]
+// console.log(alunos);
+// console.log(`O segundo aluno é ${alunos[1]}!`);
+// console.log("\n")
+// for (i=0; i<alunos.length ; ++i) {
+//     let item = `${i+1} - ${alunos[i]} \n`;
+//     console.log(`O aluno impresso é : ${item}`);
+// }
+// console.log("\n");
+// alunos.forEach((nome)=>{
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+// console.log("\n");
+// alunos.forEach(function(nome){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+// console.log("\n");
+// for (const nome of alunos){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// }
+
+// console.log("\n");
+// for (const indice in alunos){
+//     const item = `Aluno ${alunos[indice]}`;
+//     console.log(item);
+// }
