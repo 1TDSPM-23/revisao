4e3ca8374cade625289f18a7491aac491d1abf18
leocscotti <leoscotti03@gmail.com>
2023-08-04 09:32:23 -0300

    Declarando arrays, atualizando

---
+++ pf0670/js/script.js
@@ -0,0 +1,39 @@
+// // console.log("Ola mundo")
+// //Declarando um array de nomes de alunos da FIAP:
+// let alunos= [
+//     "João","Pedro","Robson", "Jorge", "Luis", "Marieta", "Enzo"
+// ]
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Imprimindo um elemento do array e apresentando com Template String:
+// console.log(`O segundo aluno é ${alunos[1]}`);
+// //Quebrando linha
+// console.log("\n");
+
+// //Imprimindo o array com for:
+// //for (i=0; i=alunos.length; ++i) {
+//     //Apresemtação dos elementos do Array:
+//   //  let item= `${i+1} - ${alunos{i}} \n`;
+//     //console.log(`O aluno impresso é: ${item}`);
+// //}
+
+// console.log("\n");
+
+// //Imprimindo o array com forEach e utilizando arrow function:
+// alunos.forEach((nome)=>{
+//     const item= `Aluno ${nome}`;
+//     console.log(item);
+// });
+// console.log("\n");
+
+// //Imprimindo o array com for of:
+// for (const nome of alunos){
+//     const item= `Aluno ${nome}`;
+//     console.log(item);
+// }
+// console.log("\n");
+// //Imprimindo o array com for in:
+// for (const nome in alunos){
+//     const item= `Aluno ${alunos[indice]}`;
+//     console.log(item);
+// }
