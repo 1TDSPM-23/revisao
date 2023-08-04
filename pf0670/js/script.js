6d47f88094ae39d488f218db99487a4c4469b13e
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-08-04 09:32:59 -0300

    Declarando arrays, iterando e acessando

---
+++ pf0670/js/script.js
@@ -0,0 +1,50 @@
+// console.log("Olá Mundo!");
+//Declarando um array de nomes de alunos da FIAP:
+// let alunos = [
+//     "João", "Pedro", "Robson", "Jorge", "Luis", "Marieta", "Enzo"
+// ]
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Imprimindo um elemento do array e apresentando com Template String:
+// console.log(`O segundo aluno é ${alunos[1]}`);
+// //Quebrando linha
+// console.log("\n");
+
+// //Imprimindo o array com for:
+// for (i=0; i<alunos.length ; ++i) {
+//     //Apresentação dos elementos do Array:
+//     let item = `${i+1} - ${alunos[i]} \n`;
+//     console.log(`O aluno impresso é : ${item}`);
+// }
+
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o array com forEach e utilizando arrow function:
+// alunos.forEach((nome)=>{
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o array com forEach e utilizando fuunção anônima
+// alunos.forEach(function(nome){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o array com for of:
+// for (const nome of alunos){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// }
+
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o array com for in:
+// for (const indice in alunos){
+//     const item = `Aluno ${alunos[indice]}`;
+//     console.log(item);
+// }
