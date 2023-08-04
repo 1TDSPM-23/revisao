2a400af47164c838af37b5184cc45f1e0404919c
biasvestka <biasvestka@gmail.com>
2023-08-04 09:32:34 -0300

    Controlando Arrays, iterando e acessando.

---
+++ pf0670/js/script.js
@@ -0,0 +1,45 @@
+// console.log("Olá Mundo!");
+// Declarando um array de nomes de alunos a FIAP:
+// let alunos = [
+//     "João", "Pedro", "Robson", "Jorge", "Luis", "Marieta", "Enzo"
+// ]
+// //Imprimindo p array completo:
+// console.log(alunos);
+// //Imprimindo um elemento do array e apresentando com Templates String:
+// console.log(`O segundo aluno é ${alunos[1]}`);
+// //Quebrando linha
+// console.log("\n")
+
+// //Imprimindo array com for:
+// for (i=0; i<alunos.lenght ; ++i) {
+//     //Apresentação dos elementos do Array:
+//     let item = `${i+1} - ${alunos[i]}\n`;
+//     console.log(`O aluno impresso é: ${item}`)
+// }
+
+// //Quebrando linha
+// console.log("\n")
+// //Imprimindo o array com forEach e utilizando arrow function:
+// alunos.forEach((nome)=>{
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o array com forEach e utilizando fuunção anônima
+// alunos.forEach(function(nome){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+
+// //Imprimindo Array com for of:
+// for (const nome of alunos){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);    
+// } 
+// //Imprimindo Array com for in:
+// for (const nome in alunos){
+//     const item = `Aluno ${alunos[indice]}`;
+//     console.log(item);    
+// } 
