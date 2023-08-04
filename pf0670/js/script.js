3ffaa2acb71ffd627170b668492894ca73a3ea21
freitaslivia <xliviafreitasx@gmail.com>
2023-08-04 09:32:33 -0300

    Declarando arrays

---
+++ pf0670/js/script.js
@@ -0,0 +1,39 @@
+// // console.log("ola mundo")
+// //Declarando um array de nomes de alunos da FIAP:
+// let alunos = [
+//     "João", "Pedro", "Robson", "Jorge", "Maria", "Enzo"
+// ]
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Imprimindo um elemento do array:
+// console.log(`O segundo aluno é ${alunos[1]}`);
+// //Quebrando linha
+// console.log("\n");
+
+// //Imprimindo o array com for
+// for (i=0; i<alunos.length ; ++i) {
+//     //Apresentação dos elementos do Array:
+//     let item = `${i+1} - ${alunos[i]} \n`;
+//     console.log(`O Aluno impresso é : ${item}`)
+// }
+
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o array com forEach e utilizando arrow function:
+// alunos.forEach((nome)=>{
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+// //Imprimindo o array com for of:
+// for (const nome of alunos) {
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// }
+
+// //Imprimindo o array com for in:
+
+// for (const indice in alunos){
+//     const item = `Aluno ${alunos[indice]}`;
+//     console.log(item);
+// }
