4ac7ac6bd44b7dc6c8029de3390162b1373ae330
Allan-Perca <a.percario@hotmail.com>
2023-08-04 09:32:32 -0300

    Declarando arrays e js

---
+++ pf0670/js/script.js
@@ -0,0 +1,53 @@
+//console.log("Olá mundo")
+//Declarando um array de nomes de alunos da FIAP
+// let alunos = [
+//     "João", "Pedro", "Robson", "Jorge", "Luis", "Marieta", "Enzo"
+// ]
+
+//Imprimindo o array completo
+//console.log(alunos);
+
+//Imprimindo um elemento do array e apresentando com Template String:
+//console.log(`O segundo aluno é ${alunos[1]}`);
+
+//Quebrando linha
+//console.log("\n");
+
+//Imprimindo o array com for
+//for (i=0; i=alunos.length; ++i){
+    //apresentação de elementos do array:
+//    let item = `${i+1} - ${alunos[i]} \n`;
+//    console.log(`O aluno impresso é: ${item}`);
+//}
+
+//imprimindo o array com forEach e utilizando arrow function
+// alunos.forEach((nome)=>{
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+//imprimindo o array com forEach e utilizando função anonima, unica coisa que vai mudar é q vai escreve function e tira arrow function
+// alunos.forEach(function(nome){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// });
+
+//imprimindo o array com for of:
+// for (const nome of alunos){
+//     const item = `Aluno ${nome}`;
+//     console.log(item);
+// }
+
+//Quebrando linha
+//console.log("\n");
+
+//Imprimindo o array com for in:
+// for (const i in alunos){
+//     const item = `Aluno ${alunos[indice]}`;
+//     console.log(item);
+// }
+
+
+
+
+
