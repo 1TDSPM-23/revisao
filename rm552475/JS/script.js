19706bcac9bb102197d522007df05aff41d56bb1
luizPrendin <luizprendinfelipe@gmail.com>
2023-08-04 09:31:59 -0300

    Declarando arrays e acessando os seus elementos

---
+++ rm552475/JS/script.js
@@ -0,0 +1,54 @@
+// // console.log("ola mundo")
+
+// //declarando arrays
+
+// let alunos = ["João","Pedro","Robson","Jorge","Luiz","Marieta","Enzo"]
+
+// console.log(alunos)
+
+// //acessando elementos do array
+// console.log(`O primeiro elemento é ${alunos[0]}`)
+// //Quebrando linhas
+// console.log("\n")
+
+// //imprimindo array  com for
+
+// for (i=0 ; i< alunos.length ; i++){
+// //apresentação dos elementos do array    
+//     let item = `${i+1}) - ${alunos[i]} \n`
+//     console.log(`O aluno impresso é : ${item}`)
+// }
+// console.log("\n")
+
+// //imprimindo os array com array com foreach e utilizando arrow func
+
+// alunos.forEach((nome)=>{
+//     const item= `Aluno ${nome}`;
+//     console.log(item) 
+// })
+// alunos.forEach(function(nome){
+//     const item = `Aluno ${nome}` 
+//     console.log(item)
+// })
+
+// console.log("\n")
+
+// function soma (a,b){
+//     return a*2 , b*3;
+// }
+
+// const soma2 =(a,b)=>{a*2,b*3}
+
+
+// console.log("\n")
+// //imprimindo array com forOf
+
+// for(const nome of alunos){
+//     const item= `Aluno ${nome}`
+//     console.log(item)
+// }
+
+// for(const indice in alunos){
+//     const item= `Aluno ${indice}`
+//     console.log(item)
+// }
