7190edf9714e14371853317a3eabca52c41f0a51
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-07 11:32:12 -0300

    Métodos(push,pop,unshift,shift,indeOf,splice,sort,reverse)

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -49,51 +49,134 @@
 //     console.log(item);
 // }
 
-//Declarar um array de Objetos de alunos:
-let alunos = [
-    {nome:"João",nota:8,situacao:"Aprovado"},
-    {nome:"Maria",nota:3,situacao:"Reprovado"},
-    {nome:"Pedro",nota:7,situacao:"Aprovado"},
-    {nome:"Ana",nota:2,situacao:"Reprovado"},
-    {nome:"Jose",nota:5,situacao:"Recuperação"}
-];
+// //Declarar um array de Objetos de alunos:
+// let alunos = [
+//     {nome:"João",nota:8,situacao:"Aprovado"},
+//     {nome:"Maria",nota:3,situacao:"Reprovado"},
+//     {nome:"Pedro",nota:7,situacao:"Aprovado"},
+//     {nome:"Ana",nota:2,situacao:"Reprovado"},
+//     {nome:"Jose",nota:5,situacao:"Recuperação"}
+// ];
 
-//Imprindo o array de objetos:
+// //Imprindo o array de objetos:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
+
+// //Imprimindo um único Objeto do array:
+// console.log(`Aluno ${alunos[0].nome} - ${alunos[0].nota} - ${alunos[0].situacao}`);
+// //Quebrando linha
+// console.log("\n");
+
+
+// //Imprimindo a lista de objetos com forEach:
+// alunos.forEach( aluno=>{
+//     if(aluno["situacao"] == "Recuperação"){
+//         console.log(`O aluno ${aluno["nome"]} tirou a nota ${aluno["nota"]} ficou de ${aluno["situacao"]}`)    
+//     }else{
+//         console.log(`O aluno ${aluno["nome"]} tirou a nota ${aluno["nota"]} e está ${aluno["situacao"]}`)
+//     }
+// });
+// //Quebrando linha
+// console.log("\n");
+
+// //Recuperando o elemento div da página:
+// const divLista = document.getElementById("lista-nomes");
+
+// //Imprimindo a lista de objetos com forEach e apresentando na página inserindo um elemento HTML dentro da div:
+// // alunos.forEach( (aluno)=>{
+// //     divLista.innerHTML += `<p>Aluno ${aluno["nome"]} - ${aluno["nota"]} - ${aluno["situacao"]}</p>`;
+// // });
+
+// //Imprimindo a lista de objetos com forEach e apresentando na página inserindo um elemento HTML dentro da div:
+// alunos.forEach( (aluno,index)=>{
+//     //Criando um elemento p no HTML:
+//     let p = document.createElement("p");
+//     //Inserindo o texto no elemento p:
+//     p.textContent = `Aluno ${aluno["nome"]} - ${aluno["nota"]} - ${aluno["situacao"]}`;
+//     //Inserindo o elemento p dentro da div:
+//     divLista.appendChild(p);
+
+//     //Imprimir o id do elemento pai do elemento p:
+//     console.log(index + " "+ p.parentElement.id);
+// });
+
+// //Quebrando linha
+// console.log("\n");
+
+
+//Declarando um array de nomes de alunos:
+let alunos = [
+    "João", "Pedro", "Robson", "Jorge", "Luis", "Marieta", "Enzo"
+]
+
+//Inserindo um elemento no final do array com o método push:
+alunos.push("Maria");
+
+//Inserindo um elemento no início do array com o método unshift:
+alunos.unshift("José");
+
+//Imprimindo o array completo:
+console.log(alunos);
+
+//Removendo o último elemento do array com o método pop:
+alunos.pop();
+
+//Imprimindo o array completo:
+console.log(alunos);
+
+//Removendo o primeiro elemento do array com o método shift:
+alunos.shift(); 
+//Imprimindo o array completo:
+console.log(alunos);
+
+//Quebrando linha
+console.log("\n");
+
+//Colocando o array em ordem alfabética com o método sort:
+alunos.sort();
+//Imprimindo o array completo:
 console.log(alunos);
 //Quebrando linha
 console.log("\n");
 
-//Imprimindo um único Objeto do array:
-console.log(`Aluno ${alunos[0].nome} - ${alunos[0].nota} - ${alunos[0].situacao}`);
+//Colocando o array em ordem inversa com o método reverse:
+alunos.reverse();
+//Imprimindo o array completo:
+console.log(alunos);
 //Quebrando linha
 console.log("\n");
 
 
-//Imprimindo a lista de objetos com forEach:
-alunos.forEach( aluno=>{
-    if(aluno["situacao"] == "Recuperação"){
-        console.log(`O aluno ${aluno["nome"]} tirou a nota ${aluno["nota"]} ficou de ${aluno["situacao"]}`)    
-    }else{
-        console.log(`O aluno ${aluno["nome"]} tirou a nota ${aluno["nota"]} e está ${aluno["situacao"]}`)
-    }
-});
+//Utilizando o método indexOf para encontrar a posição de um elemento no array:
+let posicao = alunos.indexOf("juca");
+console.log(`O aluno Juca está na posição ${posicao}`);
 //Quebrando linha
 console.log("\n");
 
-//Recuperando o elemento div da página:
-const divLista = document.getElementById("lista-nomes");
+//Utilizando o método splice para alterar um elemento do array:
+alunos.splice(1,1,"Juca");
+//Imprimindo o array completo:
+console.log(alunos);
+//Quebrando linha
+console.log("\n");
 
-//Imprimindo a lista de objetos com forEach e apresentando na página inserindo um elemento HTML dentro da div:
-// alunos.forEach( (aluno)=>{
-//     divLista.innerHTML += `<p>Aluno ${aluno["nome"]} - ${aluno["nota"]} - ${aluno["situacao"]}</p>`;
-// });
+//Utilizando o método splice para alterar eu remover um elemento do array:
+alunos.splice(1,2,"Arnaldo");
+//Imprimindo o array completo:
+console.log(alunos);
+//Quebrando linha
+console.log("\n");
 
-//Imprimindo a lista de objetos com forEach e apresentando na página inserindo um elemento HTML dentro da div:
-alunos.forEach( (aluno)=>{
-    //Criando um elemento p no HTML:
-    let p = document.createElement("p");
-    //Inserindo o texto no elemento p:
-    p.textContent = `Aluno ${aluno["nome"]} - ${aluno["nota"]} - ${aluno["situacao"]}`;
-    //Inserindo o elemento p dentro da div:
-    divLista.appendChild(p);
-});
+//Utilizando o método splice remover um elemento do array:
+alunos.splice(3,1);
+//Imprimindo o array completo:
+console.log(alunos);
+//Quebrando linha
+console.log("\n");
+
+alunos.splice(alunos.indexOf("Enzo"),1);
+//Imprimindo o array completo:
+console.log(alunos);
+//Quebrando linha
+console.log("\n");
