c77a1999728bbd3489dea19650f05897337618ff
TomazPecoraro <Tomazpecoraro0@gmail.com>
2023-08-07 12:13:15 -0300

    Botão/Pesquisa JS

--- rm98499/js/script.js
+++ rm98499/js/script.js
@@ -180,3 +180,21 @@ alunos.splice(alunos.indexOf("Enzo"),1);
 console.log(alunos);
 //Quebrando linha
 console.log("\n");
+
+const botaoPesquisar = document.getElementById("btnProcurar");
+
+botaoPesquisar.addEventListener("click", (evt)=>{;
+
+evt.preventDefault();
+
+
+    //Recuperando o valor do input:
+    const nome = document.getElementById("idNmAl").value;
+
+    //Verificando se o nome existe no array:
+    if(alunos.indexOf(nome) != - 1){
+        alert(`O aluno ${nome} está na lista`);
+    }else{
+        alert(`O aluno ${nome} não está na lista`);
+    }
+});
