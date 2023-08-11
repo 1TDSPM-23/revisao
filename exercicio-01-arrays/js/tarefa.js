38347aa1c73e0debd17ca1cbefe95265b40f7da2
DuduViolante <dudu.violant@gmail.com>
2023-08-11 09:26:21 -0300

    att

--- exercicio-01-arrays/js/tarefa.js
+++ exercicio-01-arrays/js/tarefa.js
@@ -1,3 +1,4 @@
+
 let listaTarefasArray = [];
 
 //const botaAddTarefa = document.getElementById("btnAddTarefa");
@@ -15,7 +16,34 @@ botaAddTarefa.addEventListener("click",( evt )=>{
 
     let li = document.createElement("li");
 
-    
+    li.textContent = tarefaInput.value;
+
+    let botaoExcluir = document.createElement("button");
+
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+
+    listaTarefasUL.appendChild(li);
+
+    botaoExcluir.addEventListener("click", (evt)=>{
+        //Pegando o texto do Li e colocando em uma variável
+        let conteudoDoLi = evt.target.parentNode.textContent.split(" ");
+        // console.log(conteudoDoLi[0]);
+        //Localizando o indice do elemento a ser removido
+        let indice = listaTarefasArray.indexOf(conteudoDoLi[0])
+
+        listaTarefasArray.splice()
+        
+
+        
+       
+
+        //Removendo o elemento li do HTML
+        evt.target.parentNode.remove();
+
+         // console.log(evt.target.parentNode.textContent);
+    });
 
     console.log(listaTarefasArray);
     tarefaInput.value = "";
