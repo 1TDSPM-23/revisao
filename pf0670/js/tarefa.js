0dc2f6e7f8d240b8c473fd68ad8eb61b0bf8ea19
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-11 09:32:16 -0300

    Finalizando o exercício com exclusão da tarefa, utilizando parenNode + splice e split.

--- pf0670/js/tarefa.js
+++ pf0670/js/tarefa.js
@@ -26,7 +26,21 @@ botaAddTarefa.addEventListener("click",( evt )=>{
 
     listaTarefasUL.appendChild(li);
 
-    
+    botaoExcluir.addEventListener("click", (evt)=>{
+        //Pegando o texto do Li e colocando em uma variável
+        let conteudoDoLi = evt.target.parentNode.textContent.split(" ");
+        // console.log(conteudoDoLi[0]);
+        //Localizando o indice do elemento a ser removido
+        let indice = listaTarefasArray.indexOf(conteudoDoLi[0])
+
+        //Removendo o indice selecionado com o método splice
+        listaTarefasArray.splice(indice,1);
+        
+        //Removendo o elemento li do HTML
+        evt.target.parentNode.remove();
+
+        console.log(listaTarefasArray);
+    });
 
     console.log(listaTarefasArray);
     tarefaInput.value = "";
