40a6d81463de8c166e5fd4b92ce1327c0a2fbde1
gallone10 <enzo.gallone83@gmail.com>
2023-08-11 09:30:00 -0300

    exercicio pronto

--- rm551754/js/tarefa.js
+++ rm551754/js/tarefa.js
@@ -1,9 +1,10 @@
 
 let listaTarefasArray = [];
 
-//const botaoAddTarefa = document.getElementById("btnAddTarefa");
+//const botaAddTarefa = document.getElementById("btnAddTarefa");
+const botaAddTarefa = document.querySelector("#btnAddTarefa");
 
-btnAddTarefa.addEventListener("click",( evt )=>{
+botaAddTarefa.addEventListener("click",( evt )=>{
 
     evt.preventDefault();
 
@@ -18,14 +19,30 @@ btnAddTarefa.addEventListener("click",( evt )=>{
     li.textContent = tarefaInput.value;
 
     let botaoExcluir = document.createElement("button");
-
     botaoExcluir.textContent = " x ";
 
     li.appendChild(botaoExcluir);
 
     listaTarefasUL.appendChild(li);
 
+    botaoExcluir.addEventListener("click", (evt)=>{
+        //Pegando o texto do Li e colocando em uma variável
+        let conteudoDoLi = evt.target.parentNode.textContent.split(" ");
+        // console.log(conteudoDoLi[0]);
+        //Localizando o indice do elemento a ser removido
+        let indice = listaTarefasArray.indexOf(conteudoDoLi[0])
+
+        //Removendo o indice selecionado com o método splice
+        listaTarefasArray.splice(indice,1);
+        
+        //Removendo o elemento li do HTML
+        evt.target.parentNode.remove();
+
+        console.log(listaTarefasArray);
+    });
+
     console.log(listaTarefasArray);
     tarefaInput.value = "";
     
 });
+    
