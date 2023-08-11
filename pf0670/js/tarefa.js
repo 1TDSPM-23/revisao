bef9551c3fc473dd33d85621c9012af7075e96ab
gbalttazar <gbalttazar.sit@gmail.com>
2023-08-11 09:32:11 -0300

    finalizando o exercicio com exclusão da taref, utilizando pareNode + splice e split.

--- pf0670/js/tarefa.js
+++ pf0670/js/tarefa.js
@@ -27,13 +27,19 @@ botaAddTarefa.addEventListener("click",( evt )=>{
     listaTarefasUL.appendChild(li);
 
     botaoExcluir.addEventListener("click", (evt)=>{
-        let conteudoDoLi = evt.target.parentNode.textContent
-       
+        //Pegando o texto do Li e colocando em uma variável
+        let conteudoDoLi = evt.target.parentNode.textContent.split(" ");
+        // console.log(conteudoDoLi[0]);
+        //Localizando o indice do elemento a ser removido
+        let indice = listaTarefasArray.indexOf(conteudoDoLi[0])
 
+        //Removendo o indice selecionado com o método splice
+        listaTarefasArray.splice(indice,1);
+        
         //Removendo o elemento li do HTML
         evt.target.parentNode.remove();
 
-         // console.log(evt.target.parentNode.textContent);
+        console.log(listaTarefasArray);
     });
 
     console.log(listaTarefasArray);
