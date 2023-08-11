57eb6e7059844a4142e4d39a7680ad3719108457
nichol6s <nicholas.jake15@gmail.com>
2023-08-11 09:04:29 -0300

    adicionando as coisas com botao

---
+++ rm551809/js/tarefa.js
@@ -0,0 +1,33 @@
+
+let listaTarefasArray = [];
+
+//const botaAddTarefa = document.getElementById("btnAddTarefa");
+const botaAddTarefa = document.querySelector("#btnAddTarefa");
+
+botaAddTarefa.addEventListener("click",( evt )=>{
+
+    evt.preventDefault();
+
+    const tarefaInput = document.querySelector("#idTarefa");
+
+    listaTarefasArray.push(tarefaInput.value)
+
+    const listaTarefasUL = document.querySelector("#lista-tarefas");
+
+    let li = document.createElement("li");
+
+    li.textContent = tarefaInput.value;
+
+    let botaoExcluir = document.createElement("button");
+
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+
+    listaTarefasUL.appendChild(li);
+
+    
+    console.log(listaTarefasArray);
+    tarefaInput.value = "";
+    
+});
