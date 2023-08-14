241a29e7e084f6e6048cb484436c9e4f21b0ed07
leocscotti <leoscotti03@gmail.com>
2023-08-14 14:17:46 -0300

    Exemplo final de Objetos e listagem com SPrEAD,  e Destructuring.

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -105,95 +105,325 @@
 // console.log("\n");
 
 
+
+
+// //Inserindo um elemento no final do array com o método push:
+// alunos.push("Maria");
+
+// //Inserindo um elemento no início do array com o método unshift:
+// alunos.unshift("José");
+
+// //Imprimindo o array completo:
+// console.log(alunos);
+
+// //Removendo o último elemento do array com o método pop:
+// alunos.pop();
+
+// //Imprimindo o array completo:
+// console.log(alunos);
+
+// //Removendo o primeiro elemento do array com o método shift:
+// alunos.shift(); 
+// //Imprimindo o array completo:
+// console.log(alunos);
+
+// //Quebrando linha
+// console.log("\n");
+
+// //Colocando o array em ordem alfabética com o método sort:
+// alunos.sort();
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
+
+// //Colocando o array em ordem inversa com o método reverse:
+// alunos.reverse();
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
+
+
+// //Utilizando o método indexOf para encontrar a posição de um elemento no array:
+// let posicao = alunos.indexOf("juca");
+// console.log(`O aluno Juca está na posição ${posicao}`);
+// //Quebrando linha
+// console.log("\n");
+
+// //Utilizando o método splice para alterar um elemento do array:
+// alunos.splice(1,1,"Juca");
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
+
+// //Utilizando o método splice para alterar eu remover um elemento do array:
+// alunos.splice(1,2,"Arnaldo");
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
+
+// //Utilizando o método splice remover um elemento do array:
+// alunos.splice(3,1);
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
+
+// alunos.splice(alunos.indexOf("Enzo"),1);
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
+
 //Declarando um array de nomes de alunos:
-let alunos = [
-    "João", "Pedro", "Robson", "Jorge", "Luis", "Marieta", "Enzo"
-]
+// let alunos = [
+//     "João", "Pedro", "Robson", "Jorge", "Luis", "Marieta", "Enzo"
+// ]
 
-//Inserindo um elemento no final do array com o método push:
-alunos.push("Maria");
+// //Imprimindo o array completo:
+// console.log(alunos);
+// //Quebrando linha
+// console.log("\n");
 
-//Inserindo um elemento no início do array com o método unshift:
-alunos.unshift("José");
+// const botaoPesquisar = document.getElementById("btnProcurar");
 
-//Imprimindo o array completo:
-console.log(alunos);
+// botaoPesquisar.addEventListener("click",(evt)=>{
+//     console.log(evt.target);
 
-//Removendo o último elemento do array com o método pop:
-alunos.pop();
+//     evt.preventDefault();
 
-//Imprimindo o array completo:
-console.log(alunos);
+//     //Recuperando o valor do input:
+//     const nome = document.getElementById("idNmAl").value;
 
-//Removendo o primeiro elemento do array com o método shift:
-alunos.shift(); 
-//Imprimindo o array completo:
-console.log(alunos);
+//     //Verificando se o nome existe no array:
+//     if(alunos.indexOf(nome) != -1){
+//         alert(`O aluno ${nome} está na lista`);
+//     }else{
+//         alert(`O aluno ${nome} não está na lista`);
+//     }
+// });
 
+//imprimindo o array com o método forEach:
+// let novoArrayComIndicesPares = alunos.forEach( (aluno,index)=>{
+//     if((index+1) % 2 == 0){
+//         console.log(`${index+1} - ${aluno}`);
+//         return `${index+1} - ${aluno}`;
+//     }
+// });
+// //Quebrando linha
+// console.log("\n");
+
+// //imprimindo o array com o método map:
+// novoArrayComIndicesPares.forEach( (aluno)=>{
+//     console.log(aluno);
+// });
+// //Quebrando linha
+// console.log("\n");
+
+// //imprimindo o array com o método map:
+// let novoArrayComIndicesPares = alunos.map( (aluno,index)=>{
+//     if((index+1) % 2 == 0){
+//         console.log(`${index+1} - ${aluno}`);
+//         return `${index+1} - ${aluno}`;
+//     }
+// });
+// //Quebrando linha
+// console.log("\n");
+
+// //imprimindo o novo array com o método filter:
+// novoArrayUndefined = novoArrayComIndicesPares.filter((aluno)=> aluno != undefined);
+// //Quebrando linha
+// console.log("\n");
+
+// //imprimindo o novo array com o método filter:
+// novoArrayUndefined.forEach((aluno)=> console.log(aluno));
+// //Quebrando linha
+// console.log("\n");
+
+// //Filtrando o array com o método filter utilizando o map:
+// let novoArrayComIndicesPares2 = alunos.map( (aluno,index)=>{
+//     if((index+1) % 2 == 0){
+//         console.log(`${index+1} - ${aluno}`);
+//         return `${index+1} - ${aluno}`;
+//     }
+// }).filter((aluno)=> aluno != undefined);
+// //Quebrando linha
+// console.log("\n");
+
+//Criando um objeto Pessoa:
+
+// let pessoa = {
+//     nome: "João",
+//     idade: 20,
+//     genero: "M",
+//     altura: 1.80,
+//     peso: 80,
+//     andando: false,
+//     caminhouQuantosMetros: 0,
+//     fazerAniversario: function(){
+//         this.idade++;
+//     },
+//     andar: function(metros){
+//         this.caminhouQuantosMetros += metros;
+//         this.andando = true;
+//     },
+//     parar: function(){
+//         this.andando = false;
+//     }
+// }
+
+// //Imprimindo o objeto completo:
+// console.log(pessoa);
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo um dos atributos do objeto:
+// console.log(pessoa.nome);
+// //Quebrando linha
+// console.log("\n");
+// //Utilizando o método fazerAniversario:
+// pessoa.fazerAniversario();
+// //Imprimindo o objeto completo:
+// console.log(pessoa);
+// //Quebrando linha
+// console.log("\n");
+// //Utilizando o método andar:
+// pessoa.andar(100);
+// //Imprimindo o objeto completo:
+// console.log(pessoa);
+// //Quebrando linha
+// console.log("\n");
+// //Utilizando o método parar:
+// pessoa.parar();
+// //Imprimindo o objeto completo:
+// console.log(pessoa);
+// //Quebrando linha
+// console.log("\n");
+
+// //Criando um objeto Pessoa:
+// const pessoa = {
+//     nome: "João",
+//     idade: 20,
+//     genero: "M",
+//     altura: 1.80,
+//     peso: 80,
+// }
+
+// //Criando um objeto aluno:
+// const aluno = {
+//     nota: 10,
+//     faltas: 2,
+//     curso: "Full Stack",
+//     turno: "Manhã",
+// }
+
+// //Criando um objeto professor:
+// const professor = {
+//     especialidade: "Programação",
+//     quantidadeDeAlunos: 10,
+//     salario: 1000,
+// }
+
+// //Operador Spread para copiar os atributos de um objeto para outro:
+// const pessoaAluno = {...pessoa,...aluno};
+// const pessoaProfessor = {...pessoa,...professor};
+// //Imprimindo o objeto completo:
+// console.log(pessoaAluno);
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o objeto completo:
+// console.log(pessoaProfessor);
+// //Quebrando linha
+// console.log("\n");
+
+// //Operador Spread para copiar os atributos de um objeto para outro, alterando o valor de um atributo: nome
+// const pessoaAluno2 = {...pessoa,nome:"Pedro",...aluno};
+// const pessoaProfessor2 = {...pessoa,nome:"Robson",...professor};
+// //Imprimindo o objeto completo:
+// console.log(pessoaAluno2);
+// //Quebrando linha
+// console.log("\n");
+// //Imprimindo o objeto completo:
+// console.log(pessoaProfessor2);
+// //Quebrando linha
+// console.log("\n");
+
+// //Utilizando o rest operator para criar um único objeto a partir de pessoa e aluno:
+// const pessoaAluno3 = {...pessoa,...aluno,nota:2,faltas:10};
+// //Imprimindo o objeto completo:
+// console.log(pessoaAluno3);
+// //Quebrando linha
+// console.log("\n");
+
+// //Utilizando Desestruturação para criar um único objeto a partir de pessoa e aluno:
+// const {nota:nota2,faltas:falta2, ...restantanteDoObj} = pessoaAluno3;
+// //Imprimindo as variáveis:
+// console.log(nota2);
+// console.log(falta2);
+// console.log(restantanteDoObj);
+// //Quebrando linha
+// console.log("\n");
+
+//Criando um array de objetos alunos com notas e faltas:
+// const alunos1TDSPM = [
+//     {nome: "João",nota: 10,faltas: 2},
+//     {nome: "Pedro",nota: 8,faltas: 5},
+//     {nome: "Robson",nota: 9,faltas: 0},
+//     {nome: "Lucas",nota: 5,faltas: 3},
+//     {nome: "Maria",nota: 7,faltas: 4},
+//     {nome: "Ana",nota: 6,faltas: 1},
+//     {nome: "Paulo",nota: 4,faltas: 6},
+//     {nome: "Carlos",nota: 3,faltas: 7},
+//     {nome: "Marcos",nota: 2,faltas: 8},
+//     {nome: "Ricardo",nota: 1,faltas: 9},
+// ];
+
+// //Computando a média das notas com o método reduce:
+// let mediaNotas = alunos1TDSPM.reduce((acumulador,aluno)=>{
+//     return acumulador + aluno.nota;
+// },0)/alunos1TDSPM.length;
+// //Imprimindo a média das notas:
+// console.log(mediaNotas);
+// //Quebrando linha
+// console.log("\n");
+
+// //Criando um array de notas:
+// let arrayNotas = alunos1TDSPM.map((aluno)=> aluno.nota);
+// //Imprimindo o array de notas:
+// console.log(arrayNotas);
+// //Quebrando linha
+// console.log("\n");
+
+// //Computando a média das notas com o método reduce:
+// // 10, 8, 9, 5, 7, 6, 4, 3, 2, 1
+// arrayNotas = arrayNotas.reduce((acumulador,nota)=>{
+//     return acumulador + nota;
+// },0)/arrayNotas.length;
+// //Imprimindo a média das notas:
+// console.log(arrayNotas);
+// //Quebrando linha
+// console.log("\n");
+
+
+//Criando um array de objetos alunos com os atributos nome, nota, faltas e professor:
+const alunos1TDSPM = [
+    {nome: "João",nota: 10,faltas: 2,professor: "Prof Alexandre"},
+    {nome: "Pedro",nota: 8,faltas: 5,professor: "Prof Alexandre"},
+    {nome: "Robson",nota: 9,faltas: 0,professor: "Robson"},
+    {nome: "Lucas",nota: 5,faltas: 3,professor: "Prof Luis"},
+    {nome: "Maria",nota: 7,faltas: 4,professor: "Carlos"},
+    {nome: "Ana",nota: 6,faltas: 1,professor: "Prof Wanderley"},
+    {nome: "Paulo",nota: 4,faltas: 6,professor: "Prof Luis"},
+    {nome: "Carlos",nota: 3,faltas: 7,professor: "Prof Allien"},
+    {nome: "Marcos",nota: 2,faltas: 8,professor: "Prof Ronco"},
+    {nome: "Ricardo",nota: 1,faltas: 9,professor: "Jorge"}
+];
+
+//Filtrando os alunos que possuem apenas professores:
+let alunosComProfessores = alunos1TDSPM.filter((aluno)=> aluno.nota.includes("Prof"));
+//Imprimindo o array de alunos com professores:
+console.log(alunosComProfessores);
 //Quebrando linha
 console.log("\n");
-
-//Colocando o array em ordem alfabética com o método sort:
-alunos.sort();
-//Imprimindo o array completo:
-console.log(alunos);
-//Quebrando linha
-console.log("\n");
-
-//Colocando o array em ordem inversa com o método reverse:
-alunos.reverse();
-//Imprimindo o array completo:
-console.log(alunos);
-//Quebrando linha
-console.log("\n");
-
-
-//Utilizando o método indexOf para encontrar a posição de um elemento no array:
-let posicao = alunos.indexOf("juca");
-console.log(`O aluno Juca está na posição ${posicao}`);
-//Quebrando linha
-console.log("\n");
-
-//Utilizando o método splice para alterar um elemento do array:
-alunos.splice(1,1,"Juca");
-//Imprimindo o array completo:
-console.log(alunos);
-//Quebrando linha
-console.log("\n");
-
-//Utilizando o método splice para alterar eu remover um elemento do array:
-alunos.splice(1,2,"Arnaldo");
-//Imprimindo o array completo:
-console.log(alunos);
-//Quebrando linha
-console.log("\n");
-
-//Utilizando o método splice remover um elemento do array:
-alunos.splice(3,1);
-//Imprimindo o array completo:
-console.log(alunos);
-//Quebrando linha
-console.log("\n");
-
-alunos.splice(alunos.indexOf("Enzo"),1);
-//Imprimindo o array completo:
-console.log(alunos);
-//Quebrando linha
-console.log("\n");
-const botaoPesquisar = document.getElementById("btnProcurar");
-
-botaoPesquisar.addEventListener("click",(evt)=>{
-    console.log(evt.target);
-
-    evt.preventDefault();
-
-    //Recuperando o valor do input:
-    const nome = document.getElementById("idNmAl").value;
-
-    //Verificando se o nome existe no array:
-    if(alunos.indexOf(nome) != -1){
-        alert(`O aluno ${nome} está na lista`);
-    }else{
-        alert(`O aluno ${nome} não está na lista`);
-    }
-});
