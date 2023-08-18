393f66c706f13e7606a5fdbee294db936bd6ab02
NanRe1027 <amorimrenan597@gmail.com>
2023-08-18 08:06:42 -0300

    atualizando o 'exercício_salário'

--- ex_salario/script.js
+++ ex_salario/script.js
@@ -1,19 +1,19 @@
 let salarios = [1100, 2800, 1450, 2220, 10000, 1900, 4001, 2707, 1950, 3000];
 
-function aplicarAumento(salario) {
+function Aumento(salario) {
   if (salario <= 2000) {
-    return salario * 1.15; // Aumento de 15% para salários até 2000
+    return salario * 1.15; 
   } else {
-    return salario * 1.10; // Aumento de 10% para salários acima de 2000
+    return salario * 1.10; 
   }
 }
 
-let salariosAtualizados = salarios.map(aplicarAumento);
+let salariosAtualizados = salarios.map(Aumento);
 
-let salariosSuperiores2500 = salariosAtualizados.filter(salario => salario > 2500);
+let salariosAcima2500 = salariosAtualizados.filter(salario => salario > 2500);
 
-let somaSalarios = salariosSuperiores2500.reduce((acumulador, salario) => acumulador + salario, 0);
+let somaSalarios = salariosAcima2500.reduce((acumulador, salario) => acumulador + salario, 0);
 
 console.log("Salários após os aumentos:", salariosAtualizados);
-console.log("Salários superiores a 2500:", salariosSuperiores2500);
+console.log("Salários superiores a 2500:", salariosAcima2500);
 console.log("Soma dos salários superiores a 2500:", somaSalarios);
